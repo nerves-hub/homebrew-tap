f9fd45ecca1337ad7456ed4a7625fcb00f798279
Josh Kalderimis <josh.kalderimis@gmail.com>
2025-03-24 08:37:19 +0000

    Updated cli to v3.0.0

--- Formula/nh.rb
+++ Formula/nh.rb
@@ -2,14 +2,14 @@ class Nh < Formula
   desc "Command-line interface for the NervesHub platform"
   homepage "https://github.com/nerves-hub/nerves_hub_cli"
 
-  version "3.0.0-rc.1"
+  version "3.0.0"
 
   base_download_url = "https://github.com/nerves-hub/nerves_hub_cli/releases/download/v#{version}"
 
-  macos_x86_64_sha = "e53dbff2daa8bef749bb5aa830cb984b7c286462fde9fa930df2a105894dbef1"
-  macos_aarch64_sha = "81e7e3410727b2a51cae02f10fbb62316f9632086d6c1b316788f9d06d3c9eb1"
-  linux_x86_64_sha = "368fa0dbfac48de4679400e1522544511beeaeda618ed1aee15afab44e0388bb"
-  linux_aarch64_sha = "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  macos_x86_64_sha = "0afbae55145c6f475158291f8502aa203945431108358cc5ded1013c111105c8"
+  macos_aarch64_sha = "002302f852fb1b20332b84bdfa989ee9eef23463a5e780d34e7687cd170b8131"
+  linux_x86_64_sha = "0d2192641c66467b222585112fddf185649789fa58b2e8c907a1acb42b2bd315"
+  linux_aarch64_sha = "d8a6090cd2c29546c02bdcddb822e148e562d12594e734b3ccb46f122b598f8d"
 
   if OS.mac? && Hardware::CPU.intel?
     url "#{base_download_url}/macos-x86_64.tar.xz"
